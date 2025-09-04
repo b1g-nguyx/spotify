5d444e12a176316a401ca283ed427ec00900c43c
b1g_nguyx <bichnq88@gmail.com>
2025-09-04 20:38:15 +0700

    Integrate Firebase Core into the Flutter app and configure necessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify_clone_app/core/configs/theme/app_themes.dart';
+import 'package:spotify_clone_app/firebase_options.dart';
 import 'package:spotify_clone_app/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:spotify_clone_app/presentation/splash/pages/splash.dart';
 
@@ -14,6 +16,7 @@ Future<void> main() async {
         ? HydratedStorageDirectory.web
         : HydratedStorageDirectory((await getTemporaryDirectory()).path),
   );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
